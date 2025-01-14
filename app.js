f6854b3a66cfb427e8db1a2c54b0df5b85cf0dcd
Karan-Bharti1 <karan.codes1@gmail.com>
2025-01-14 13:00:02 +0530

    Nested elements inside react

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const heading=React.createElement("h1",{id: "heading"},"Hello! world from react")
+{/* <div id="parent">
+<div id"children>
+<h1></h1>
+</div>
+</div>
+Lets try to create this type of structure using React */}
+const parent=React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"children"},
+        React.createElement("h1",{id:"heading"},
+            "This is an example of nested Elements inside react")))
+console.log(parent)
 const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+root.render(parent)
